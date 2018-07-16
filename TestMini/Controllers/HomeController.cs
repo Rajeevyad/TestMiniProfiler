bbef63854368c04a5d628a1137d1858a85f3fc63
ryadav46 <ryadav46@csc.com>
2018-07-16 17:37:58 +0530

    MiniProfiler added

---
+++ TestMini/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using StackExchange.Profiling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMini.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var profiler = MiniProfiler.Current; // it's ok if this is null
+            using (profiler.Step("Set page title"))
+            {
+                ViewBag.Title = "Home Page";
+            }
+            using (profiler.Step("Doing complex stuff"))
+            {
+                using (profiler.Step("Step A"))
+                { // something more interesting here
+                    Thread.Sleep(100);
+                }
+                using (profiler.Step("Step B"))
+                { // and here
+                    Thread.Sleep(250);
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestMini/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMini.MvcApplication" Language="C#" %>
